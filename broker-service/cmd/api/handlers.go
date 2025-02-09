8468823207e039660099ff91806a1ed5d41298ab
hannesgregor <hannesgregor@users.noreply.github.com>
2025-02-09 15:20:19 +0200

    Update to .dockerfile

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
-		Error: false,
+		Error:   false,
 		Message: "Hit the broker",
-
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
 }
-
-
